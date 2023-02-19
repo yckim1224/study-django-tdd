1bfdb6afc278a7a764dc790819f982b07da9f791
Youngchai Kim <yckim1224@kigam.re.kr>
2023-02-19 23:42:02 +0900

    Add a new model "Company"

--- api/coronavstech/companies/models.py
+++ api/coronavstech/companies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Company(models.Model):
+    class CompanyStatus(models.TextChoices):
+        LAYOFFS = 'Layoffs'
+        HIRING_FREEZE = 'Hiring Freeze'
+        HIRING = 'Hiring'
+
+    name = models.CharField(max_length=30, unique=True)
+    status = models.CharField(choices=CompanyStatus.choices, default=CompanyStatus.HIRING, max_length=30)
+    last_update = models.DateTimeField(default=now, editable=True)
+    application_link = models.URLField(blank=True)
+    notes = models.CharField(max_length=100, blank=True)
+    
